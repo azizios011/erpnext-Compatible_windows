238ef22616c980e489dc72c203669ce88f068cb6
azizios011 <hdriaziz699@gmail.com>
2026-05-08 16:40:13 +0100

    refactor: remove parent workspace dependency and migrate desktop icons to fixtures

--- erpnext/tunisian_accounting/hooks.py
+++ erpnext/tunisian_accounting/hooks.py
@@ -3,3 +3,6 @@ app_title = "Tunisian Accounting"
 app_publisher = "Your Company"
 app_description = "Tunisian Accounting Module - SYSCOHADA/PCG Chart of Accounts"
 app_version = "0.0.1"
+
+fixtures = ["Desktop Icon"]
+
