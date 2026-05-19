8ee586718a516b96dc9ce9804e85d389c34091b4
azizios011 <hdriaziz699@gmail.com>
2026-05-19 11:45:20 +0100

    refactor: remove redundant Chart of Accounts link from workspace sidebar migration

--- erpnext/tunisian_accounting/after_migrate.py
+++ erpnext/tunisian_accounting/after_migrate.py
@@ -35,13 +35,6 @@ def _ensure_workspace_sidebar(workspace_name: str):
         item.link_type = "DocType"
         item.link_to = "Chart of Accounts"
         sidebar.append("items", item)
-
-        item2 = frappe.new_doc("Workspace Sidebar Item")
-        item2.label = "Chart of Accounts"
-        item2.type = "Link"
-        item2.link_type = "DocType"
-        item2.link_to = "Chart of Accounts"
-        sidebar.append("items", item2)
     else:
         item = frappe.new_doc("Workspace Sidebar Item")
         item.label = workspace_name
