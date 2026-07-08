1978deea7a5947044deadc218b49ea6d9db7002f
azizios011 <hdriaziz699@gmail.com>
2026-07-08 13:19:03 +0100

    feat: add Journals Creations Detail doctype with accounting entries integration and line number management

--- erpnext/tunisian_accounting/doctype/journals_creations/journals_creations.js
+++ erpnext/tunisian_accounting/doctype/journals_creations/journals_creations.js
@@ -2,12 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Journals Creations", {
-	onload: function (frm) {
+	onload(frm) {
 		if (frm.is_new()) {
 			frappe.call({
 				type: "GET",
 				method: "erpnext.tunisian_accounting.doctype.journals_creations.journals_creations.get_naming_series",
-				callback: function (r) {
+				callback(r) {
 					if (r.message) {
 						frm.set_df_property("naming_series", "options", r.message.split("\n"));
 						frm.set_value("naming_series", r.message.split("\n")[0]);
@@ -17,7 +17,51 @@ frappe.ui.form.on("Journals Creations", {
 			});
 		}
 	},
-	refresh: function (frm) {
+
+	setup(frm) {
+		frm.set_query("account", "accounting_entries", () => ({
+			filters: {
+				company: frm.doc.company,
+				is_group: 0,
+			},
+		}));
+
+		frm.set_query("tresorerie", "accounting_entries", () => ({
+			filters: {
+				company: frm.doc.company,
+				is_group: 0,
+				account_type: ["in", ["Bank", "Cash"]],
+			},
+		}));
+	},
+
+	refresh(frm) {
 		frappe.model.set_default_values(frm.doc);
-	}
+		update_line_numbers(frm);
+	},
+
+	company(frm) {
+		(frm.doc.accounting_entries || []).forEach((row) => {
+			row.account = "";
+			row.tresorerie = "";
+		});
+		frm.refresh_field("accounting_entries");
+	},
 });
+
+frappe.ui.form.on("Journals Creations Detail", {
+	accounting_entries_add(frm) {
+		update_line_numbers(frm);
+	},
+
+	accounting_entries_remove(frm) {
+		update_line_numbers(frm);
+	},
+});
+
+function update_line_numbers(frm) {
+	(frm.doc.accounting_entries || []).forEach((row, index) => {
+		row.line_no = index + 1;
+	});
+	frm.refresh_field("accounting_entries");
+}
