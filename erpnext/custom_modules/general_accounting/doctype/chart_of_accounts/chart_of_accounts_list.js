a9a9e0dbd311da02daa3ad146a42009da844b2b4
azizios011 <hdriaziz699@gmail.com>
2026-08-04 20:48:44 +0100

    feat: implement Chart of Accounts quick entry form with multi-step dialog

--- erpnext/custom_modules/general_accounting/doctype/chart_of_accounts/chart_of_accounts_list.js
+++ erpnext/custom_modules/general_accounting/doctype/chart_of_accounts/chart_of_accounts_list.js
@@ -1,3 +1,9 @@
 frappe.listview_settings["Chart of Accounts"] = {
 	hide_name_column: true,
+	onload: function (listview) {
+		listview.page.set_primary_action(__("Add Account"), function () {
+			listview.new_doc();
+		});
+	},
 };
+
---
+++ erpnext/custom_modules/general_accounting/doctype/chart_of_accounts/chart_of_accounts_quick_entry.js
@@ -0,0 +1,127 @@
+frappe.provide("frappe.ui.form");
+
+frappe.ui.form.ChartOfAccountsQuickEntryForm = class ChartOfAccountsQuickEntryForm extends frappe.ui.form.QuickEntryForm {
+	constructor(doctype, after_insert, init_callback, doc, force) {
+		super(doctype, after_insert, init_callback, doc, force);
+		this.selected_class = null;
+	}
+
+	render_dialog() {
+		this.mandatory = [
+			{
+				label: __("Account"),
+				fieldname: "account",
+				fieldtype: "Data",
+				reqd: 1,
+			},
+			{
+				label: __("Label"),
+				fieldname: "label",
+				fieldtype: "Data",
+				reqd: 1,
+			},
+			{
+				label: __("Account Type"),
+				fieldname: "account_type",
+				fieldtype: "Select",
+				options: "\nGroup\nLedger",
+				reqd: 1,
+			},
+			{
+				label: __("Nature"),
+				fieldname: "nature",
+				fieldtype: "Data",
+			},
+			{
+				label: __("Direction"),
+				fieldname: "direction",
+				fieldtype: "Select",
+				options: "\nDebit\nCredit",
+			},
+		];
+
+		super.render_dialog();
+		this.render_step_1();
+	}
+
+	render_step_1() {
+		const me = this;
+		this.selected_class = null;
+		this.dialog.set_title(__("Select Account Class"));
+
+		// Hide standard footer primary button and fields layout in step 1
+		this.dialog.get_primary_btn().hide();
+		if (this.dialog.custom_action_btn) {
+			this.dialog.custom_action_btn.hide();
+		}
+		$(this.dialog.body).find(".form-layout").hide();
+
+		// Hide back button if present
+		if (this.dialog.$wrapper) {
+			this.dialog.$wrapper.find(".coa-btn-back").hide();
+		}
+
+		// Create step 1 container if not existing
+		let $step1 = $(this.dialog.body).find(".coa-step-1");
+		if (!$step1.length) {
+			$step1 = $('<div class="coa-step-1"></div>').appendTo(this.dialog.body);
+		}
+		$step1.empty().show();
+
+		const classes = [
+			"Capitaux",
+			"Immobilisations",
+			"Stock et Encours",
+			"Tiers",
+			"Financiers",
+			"Charges",
+			"Produits",
+		];
+
+		const $list = $('<div class="list-group"></div>').appendTo($step1);
+
+		classes.forEach((className) => {
+			const $item = $(
+				`<a href="#" class="list-group-item list-group-item-action">${className}</a>`
+			);
+			$item.on("click", (e) => {
+				e.preventDefault();
+				me.selected_class = className;
+				me.render_step_2(className);
+			});
+			$list.append($item);
+		});
+	}
+
+	render_step_2(className) {
+		const me = this;
+		this.dialog.set_title(__("New {0} Account", [className]));
+
+		// Hide step 1 view
+		$(this.dialog.body).find(".coa-step-1").hide();
+
+		// Show standard fields layout and primary button
+		$(this.dialog.body).find(".form-layout").show();
+		this.dialog.get_primary_btn().show();
+
+		// Set or show back button in title head / header
+		let $backBtn = this.dialog.$wrapper.find(".coa-btn-back");
+		if (!$backBtn.length) {
+			$backBtn = $(
+				`<button class="btn btn-default btn-xs pull-left coa-btn-back mr-2" style="margin-right: 8px;">
+					<i class="fa fa-arrow-left"></i> ${__("Back")}
+				</button>`
+			);
+			const $header = this.dialog.$wrapper.find(".modal-header");
+			const $target = $header.find(".title-section").length
+				? $header.find(".title-section")
+				: $header;
+			$backBtn.prependTo($target);
+			$backBtn.on("click", (e) => {
+				e.preventDefault();
+				me.render_step_1();
+			});
+		}
+		$backBtn.show();
+	}
+};
