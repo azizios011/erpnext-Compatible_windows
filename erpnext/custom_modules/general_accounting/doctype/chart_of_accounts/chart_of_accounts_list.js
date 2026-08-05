4293c65c31472351c6dd182b767b651e8d82b828
azizios011 <hdriaziz699@gmail.com>
2026-08-05 10:14:56 +0100

    feat: remove unused meta HTML generation in Chart of Accounts list view

--- erpnext/custom_modules/general_accounting/doctype/chart_of_accounts/chart_of_accounts_list.js
+++ erpnext/custom_modules/general_accounting/doctype/chart_of_accounts/chart_of_accounts_list.js
@@ -31,28 +31,8 @@ frappe.listview_settings["Chart of Accounts"] = {
 			return html;
 		};
 
-		listview.get_meta_html = function (doc) {
-			const modified = comment_when(doc.modified, true);
-
-			let assigned_to = "";
-			let assigned_users = doc._assign ? JSON.parse(doc._assign) : [];
-			if (assigned_users.length) {
-				assigned_to = `<div class="list-assignments d-flex align-items-center">
-						${frappe.avatar_group(assigned_users, listview.max_number_of_avatars - 1, { filterable: true })[0].outerHTML}
-					</div>`;
-			}
-
-			return `
-				<div class="level-item list-row-activity hidden-xs">
-					<div class="hidden-md hidden-xs d-flex">
-						${assigned_to}
-					</div>
-					<span class="modified">${modified}</span>
-				</div>
-				<div class="level-item visible-xs text-right">
-					${listview.get_indicator_html(doc)}
-				</div>
-			`;
+		listview.get_meta_html = function () {
+			return "";
 		};
 
 		listview.$result.on("click", ".coa-row-add", function (e) {
