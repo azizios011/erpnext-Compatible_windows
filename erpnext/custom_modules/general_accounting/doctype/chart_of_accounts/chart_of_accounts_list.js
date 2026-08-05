0b13f7eae607087eaa7932adf819c18c5f68cbca
azizios011 <hdriaziz699@gmail.com>
2026-08-05 08:49:26 +0100

    feat: enhance Chart of Accounts list view with action buttons for adding and deleting accounts

--- erpnext/custom_modules/general_accounting/doctype/chart_of_accounts/chart_of_accounts_list.js
+++ erpnext/custom_modules/general_accounting/doctype/chart_of_accounts/chart_of_accounts_list.js
@@ -1,8 +1,32 @@
 frappe.listview_settings["Chart of Accounts"] = {
 	hide_name_column: true,
 	onload: function (listview) {
-		listview.page.set_primary_action(__("Add Account"), function () {
+		listview.page.clear_primary_action();
+
+		listview.settings.button = {
+			show: () => true,
+		};
+
+		listview.generate_button_html = function (doc) {
+			return `<div class="list-row-col hidden-xs coa-row-actions">
+				<button class="btn btn-xs btn-default coa-row-add" data-name="${doc.name}" title="${__("New Account")}">${frappe.utils.icon("plus", "sm")}</button>
+				<button class="btn btn-xs btn-default coa-row-delete" style="margin-left: 4px;" data-name="${doc.name}" title="${__("Delete")}">${frappe.utils.icon("delete", "xs")}</button>
+			</div>`;
+		};
+
+		listview.$result.on("click", ".coa-row-add", function (e) {
+			e.preventDefault();
+			e.stopPropagation();
 			listview.new_doc();
+		});
+
+		listview.$result.on("click", ".coa-row-delete", function (e) {
+			e.preventDefault();
+			e.stopPropagation();
+			let name = $(this).attr("data-name");
+			frappe.confirm(__("Delete account {0}?", [name]), () => {
+				frappe.db.delete_doc("Chart of Accounts", name).then(() => listview.refresh());
+			});
 		});
 	},
 };
